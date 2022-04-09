65161dfa4f7df0611f2da0097443431ad196de04
Chakradhar Botcha <chakradhar.botcha@gmail.com>
2022-04-09 14:29:55 -0500

    Add data access to precipitation service

--- Cloudweather.Precipitation/Program.cs
+++ Cloudweather.Precipitation/Program.cs
@@ -1,4 +1,6 @@
+using Cloudweather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
+
+builder.Services.AddDbContext<PrecipDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip,[FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async (string zip,[FromQuery] int? days, PrecipDbContext db) =>
 {
-    return Results.Ok(zip);
+    if(days == null||days<1 || days >30)
+    {
+        return Results.BadRequest("Provide a days query parameter betweeen 1 and 30");
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation.Where(precip => precip.ZipCode == zip  && precip.CreatedOn >startDate).ToListAsync();
+    return Results.Ok(results);
 
 });
 
